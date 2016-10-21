f8ccc5914a1461cd7841cf0d6d787102ae44c1fe
Martin Nally <mnally@apigee.com>
2016-10-21 16:50:11 -0700

    change environment variable names to avoid conflict with Kubernetes-allocated names

--- test/run-customers-api.sh
+++ test/run-customers-api.sh
@@ -2,10 +2,10 @@ export IPADDRESS="127.0.0.1"
 export PORT=3008
 export COMPONENT="customers"
 export SPEEDUP=10
-export EXTERNAL_ROUTER_HOST="localhost"
-export EXTERNAL_ROUTER_PORT="8080"
-export INTERNAL_ROUTER_HOST="localhost"
-export INTERNAL_ROUTER_PORT="8080"
+export EXTERNAL_SY_ROUTER_HOST="localhost"
+export EXTERNAL_SY_ROUTER_PORT="8080"
+export INTERNAL_SY_ROUTER_HOST="localhost"
+export INTERNAL_SY_ROUTER_PORT="8080"
 export INTERNAL_SCHEME="http"
 
 source test/local-export-pg-connection-variables.sh
